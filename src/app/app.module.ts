8a4f48c8a6f3aa4989fe7aa7cc22694543fb917b
Gabriel Santos <gabmaxsan@gmail.com>
2017-11-18 22:16:41 -0200

    div dos restaurantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { AboutComponent } from './about/about.component';
 
 import { ROUTES } from './app.routes';
 import { RouterModule } from '@angular/router';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
