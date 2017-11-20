93eb567457909d7ccbaa074c95630ed2e571ea6c
Gabriel Santos <gabmaxsan@gmail.com>
2017-11-20 16:10:46 -0200

    fazendo reviews

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -26,5 +26,11 @@ export class RestaurantsService {
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
     }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    }
 // tslint:disable-next-line:eofline
 }
