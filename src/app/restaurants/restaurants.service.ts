4b013dfc518661e93cc6e7743dd8e0b142f31b41
Gabriel Santos <gabmaxsan@gmail.com>
2017-11-20 16:37:34 -0200

    itens do menu

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { ErrorHandler } from '../app.error-handler';
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -31,6 +32,12 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
     }
 // tslint:disable-next-line:eofline
 }
